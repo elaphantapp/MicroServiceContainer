6c5de514719f959ee46ca994f62cb7c230b610fe
Fengyi Liu <liufengyi@jjl.cn>
2019-11-21 10:00:42 +0800

    1. Add build script to build Launcher binary and Transmitter SO library 2. Need to change SDK_DIR and SDK_INSTALL_DIR in the build_pc.sh

---
+++ build_pc.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+set -u
+
+OUTPUT_PATH=${OUTPUT_HOME:-}
+if [ -z ${OUTPUT_PATH} ]; then
+	OUTPUT_PATH="$(pwd)/output"
+fi
+echo "build path: ${OUTPUT_PATH}"
+mkdir -p  ${OUTPUT_PATH}/build
+mkdir -p  ${OUTPUT_PATH}/dict
+
+SDK_DIR="/Users/liufy/Documents/Projects/ElephantWallet/sdk/Elastos.SDK.ElephantWallet.Contact/"
+SDK_INSTALL_DIR="/Users/liufy/Documents/Projects/ElephantWallet/sdk/Elastos.SDK.ElephantWallet.Contact/build/sysroot/Darwin/x86_64"
+SDK_INCLUDE_DIR="${SDK_INSTALL_DIR}/include/"
+
+# copy Josn.hpp to install path
+if [ ! -e ${SDK_INCLUDE_DIR}/include/Json.hpp ]; then
+	echo "Copy SDK include to ${SDK_INCLUDE_DIR}"
+	#mkdir -p ${SDK_INCLUDE_DIR}
+	cp -r ${SDK_DIR}/lib/wrapper/json/*.hpp ${SDK_INCLUDE_DIR}
+fi
+
+
+loginfo() {
+	echo -e "\033[1;34m [-]: $@ \033[00m";
+}
+
+build_module() {
+	loginfo "To build module: ${1}"
+
+	src_dir="$(pwd)/${1}"
+	mkdir -p ${OUTPUT_PATH}/build/${1}
+	pushd ${OUTPUT_PATH}/build/${1}
+	cmake -DCMAKE_INSTALL_PREFIX=${OUTPUT_PATH}/dict ${2} ${src_dir}
+	make && make install
+	popd
+}
+
+build_module Launcher ""
+build_module Transmitter "-DSDK_INCLUDE_DIR=${SDK_INCLUDE_DIR} -DSDK_INSTALL_PREFIX=${SDK_INSTALL_DIR}"
